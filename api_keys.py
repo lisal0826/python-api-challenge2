c959025fe327565a87a0d7e28cc01c24a39b911d
lisal0826 <141580154+lisal0826@users.noreply.github.com>
2024-02-13 13:33:41 -0500

    update files

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,8 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "bf69921c98102f81b9a0a272099ee3d0"
+
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "f79c9b5e08794c6fbd213c09e1d6db90"
+
+
